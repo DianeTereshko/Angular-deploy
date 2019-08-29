24cabe873233dffa14e9206cf2598119600c15a6
DianeTereshko <45483517+DianeTereshko@users.noreply.github.com>
2019-08-29 23:45:17 +0500

    Initial Angular Deploy

---
+++ AngularDeploy/AngularDeploy/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace AngularDeploy.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
